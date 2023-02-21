a1db4a10d9eb74b2c2b458c0a82db0a29588db55
Swapnil Pathak <61601014+swapnilpathak14@users.noreply.github.com>
2023-02-21 10:51:12 +0530

    Update F1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -20,8 +20,21 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if (newSpeed<=50){
+            changeGear(1);
+        } else if(newSpeed<=100)
+            changeGear(2);
+        else if(newSpeed<=150)
+            changeGear(3);
+        else if(newSpeed<=200)
+            changeGear(4);
+        else if(newSpeed<=250)
+            changeGear(5);
+        else changeGear(6);
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
